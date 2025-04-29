4610f7e3c059cf9034b3f98f9cf764d31b98321e
18safarr <hibrahimakoundia@gmail.com>
2025-04-29 19:58:18 +0200

    init

---
+++ src/main/java/com/example/Moderateur/ModerateurApplication.java
@@ -0,0 +1,13 @@
+package com.example.Moderateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerateurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModerateurApplication.class, args);
+	}
+
+}
