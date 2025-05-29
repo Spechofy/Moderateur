30c7d97d502dd47145a45e23f9f0e9914a808b02
18safarr <hibrahimakoundia@gmail.com>
2025-05-29 16:47:22 +0200

    last commit
    Signed-off-by: 18safarr <hibrahimakoundia@gmail.com>

--- src/main/java/com/example/Moderateur/ModerateurApplication.java
+++ src/main/java/com/example/Moderateur/ModerateurApplication.java
@@ -2,13 +2,19 @@ package com.example.Moderateur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+/**
+ * The type Moderateur application.
+ */
 @SpringBootApplication
 public class ModerateurApplication {
-
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ModerateurApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/Moderateur/services/google_api/CommentAnalyzer.java
+++ src/main/java/com/example/Moderateur/services/google_api/CommentAnalyzer.java
@@ -1,6 +1,6 @@
 package com.example.Moderateur.services.google_api;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
